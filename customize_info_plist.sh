f1c7e4b42ccfaf62ba9913e08ea7adacf4dfea1b
Elliot Stechman <elliot.stechman@gmail.com>
2025-08-01 10:45:52 +0000

    Fix CFBundleExecutable mismatch in customize_info_plist.sh
    - Replace global string replacement with precise field-specific replacements
    - Preserve CFBundleExecutable pointing to 'Electron' while updating display names
    - Fix persistent 'damaged or incomplete' macOS app error

--- customize_info_plist.sh
+++ customize_info_plist.sh
@@ -7,7 +7,8 @@ fi
 
 INFO_PLIST="$1"
 
-sed -i 's/<string>Electron<\/string>/<string>zmNinja<\/string>/g' "$INFO_PLIST"
+sed -i 's/<key>CFBundleDisplayName<\/key>\s*<string>Electron<\/string>/<key>CFBundleDisplayName<\/key>\n\t<string>zmNinja<\/string>/g' "$INFO_PLIST"
+sed -i 's/<key>CFBundleName<\/key>\s*<string>Electron<\/string>/<key>CFBundleName<\/key>\n\t<string>zmNinja<\/string>/g' "$INFO_PLIST"
 sed -i 's/<string>com\.github\.Electron<\/string>/<string>com.zoneminder.zmninja<\/string>/g' "$INFO_PLIST"
 sed -i 's/<string>electron\.icns<\/string>/<string>icon.icns<\/string>/g' "$INFO_PLIST"
 
